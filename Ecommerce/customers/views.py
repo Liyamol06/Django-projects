0bf06ced8a07c2893b09bf3e06ec4ec7a96bae00
Liyamol <liyamol06@gmail.com>
2025-10-28 21:06:15 +0530

    Customer Registeration

--- Ecommerce/customers/views.py
+++ Ecommerce/customers/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Customers
 
 # Create your views here.
 def ShowAccount(request):
+    if request.POST and 'register' in request.POST:
+        print(request.POST)
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        address=request.POST.get('address')
+        password=request.POST.get('password')
+
+        # creating user data
+        user=User.objects.create(
+            username=name,
+            password=password,
+            email=email
+        )
+        #  creating customer data 
+        customer=Customers.objects.create(
+            name=name,
+            phone=phone,
+            address=address,
+            email=email,
+            user=user
+        )
+        return redirect('home')
+    elif request.POST and 'login' in request.POST:
+        print('-------------------------------------------------')
+        print('Login form')
+        print('-------------------------------------------------')
     return render(request, 'account.html')
