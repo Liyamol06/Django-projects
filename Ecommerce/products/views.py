c7738400988feb5f3796f7e3508aa558a4c9daea
Liyamol <liyamol06@gmail.com>
2025-10-28 07:53:26 +0530

    Pagnation added

--- Ecommerce/products/views.py
+++ Ecommerce/products/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from . models import Products
+from django.core.paginator import Paginator
 
 # Create your views here.
 def Index(request):
     return render(request, 'index.html')
 
 def ListProducts(request):
+    page=1
+    if request.GET:
+        page=request.GET.get('page')
     products_list=Products.objects.all()
-    context={
-        'products': products_list,
-        }
+    paginator=Paginator(products_list, 2)
+    products_list=paginator.page(page)
+    context={'products': products_list,}
     return render(request, 'products.html', context)
 
 def DetailedProduct(request):
