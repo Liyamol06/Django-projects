4bdc0c47909f1ab30050c080440b5d2f79a08b01
Liyamol <liyamol06@gmail.com>
2025-10-29 09:39:36 +0530

    Registration added with duplicate name messages

--- Ecommerce/customers/views.py
+++ Ecommerce/customers/views.py
@@ -1,34 +1,40 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Customers
 
 # Create your views here.
 def ShowAccount(request):
     if request.POST and 'register' in request.POST:
-        print(request.POST)
-        name=request.POST.get('name')
-        email=request.POST.get('email')
-        phone=request.POST.get('phone')
-        address=request.POST.get('address')
-        password=request.POST.get('password')
+        try:
+            name=request.POST.get('name')
+            email=request.POST.get('email')
+            phone=request.POST.get('phone')
+            address=request.POST.get('address')
+            password=request.POST.get('password')
 
-        # creating user data
-        user=User.objects.create(
-            username=name,
-            password=password,
-            email=email
-        )
-        #  creating customer data 
-        customer=Customers.objects.create(
-            name=name,
-            phone=phone,
-            address=address,
-            email=email,
-            user=user
-        )
-        return redirect('home')
+            # creating user data
+            user=User.objects.create(
+                username=name,
+                password=password,
+                email=email
+            )
+            #  creating customer data 
+            customer=Customers.objects.create(
+                name=name,
+                phone=phone,
+                address=address,
+                email=email,
+                user=user
+            )
+            return redirect('home')
+        except Exception as e:
+            error_message="Duplicate Username or Invalid Credentials"
+            messages.error(request, error_message)
+
     elif request.POST and 'login' in request.POST:
+        
         print('-------------------------------------------------')
-        print('Login form')
+        print(request.POST)
         print('-------------------------------------------------')
     return render(request, 'account.html')
